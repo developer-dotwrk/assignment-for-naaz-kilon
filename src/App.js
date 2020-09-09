e7e5ab46e2ccc48f49a14e36bb70a8666bbde728
Fedor Pushkov <f.pushkov@g.nsu.ru>
2020-09-09 16:34:03 +0700

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <div className="alert alert-primary" role="alert">
-        This is a primary alert—check it out!
-      </div>
+      <Header />
+      <div className="container"></div>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <button type="button" class="header__btn">
+        TASK
+      </button>
+    </header>
+  );
+}
